78c903acee401cb85c4de9c5c7b3e9957ffe20ce
Božo Barun <bbarun@gmail.com>
2014-04-02 11:51:02 +0200

    Finally some work

--- geocoder/src/geocoder.py
+++ geocoder/src/geocoder.py
@@ -4,5 +4,12 @@ Created on 2. 4. 2014.
 @author: barboz
 '''
 
+import re
+import requests
+
+
 if __name__ == '__main__':
-    print 'Try something out!'
+    request = requests.get('https://maps.googleapis.com/maps/api/geocode/json?address=1600+Amphitheatre+Parkway,+Mountain+View,+CA&sensor=false')
+    print request
+    print request.text
+    print request.json()
